6a1323675d57d8a410a43826a18ef1289f14f759
IvanGaray <68177252+IvanGaray@users.noreply.github.com>
2022-08-10 18:30:16 -0300

    [Act]SQLite added
    Now we can save the results in a table using sqlite.

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
+from logging import Logger, exception
 from time import sleep
 from turtle import title
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import pandas as pd
+import sqlite3
+
+
+conn = sqlite3.connect("lunch.db")
+
+c = conn.cursor()
+# c.execute("""DROP TABLE repositories""")
+c.execute("""CREATE TABLE repositories(title TEXT,language TEXT,precentage TEXT)""")
+
 
 options = webdriver.ChromeOptions()
 options.add_argument("headless")
@@ -21,7 +31,7 @@ def getElementsText(array):
 driver.get("https://github.com/IvanGaray")
 sleep(3)
 urls = []
-titles = []
+
 
 try:
     testEvidence = driver.find_elements(
@@ -31,11 +41,9 @@ try:
 
     for url in testEvidence:
         urls.append(url.get_attribute("href"))
-    for title in webTitle:
-        titles.append(title.text)
 
     print(urls)
-    print(titles)
+
     driver.save_screenshot("./evidence/main.png")
 
 except:
@@ -53,33 +61,43 @@ for url in urls:
     driver.get(url)
     sleep(3)
     try:
-        txtLanguage = driver.find_elements(
+        repoTitle = driver.find_element(
+            By.CSS_SELECTOR,
+            "strong[itemprop=name]>a",
+        ).text
+
+        mainLanguage = driver.find_element(
             By.CSS_SELECTOR,
             "div.Layout.Layout--flowRow-until-md.Layout--sidebarPosition-end.Layout--sidebarPosition-flowRow-end > div > div > div> div> ul>li:nth-child(1)>a>span:nth-child(2)",
-        )
+        ).text
 
-        txtPercentage = driver.find_elements(
+        percentage = driver.find_element(
             By.CSS_SELECTOR,
             "div.Layout.Layout--flowRow-until-md.Layout--sidebarPosition-end.Layout--sidebarPosition-flowRow-end > div > div > div> div> ul>li:nth-child(1)>a>span:nth-child(3)",
-        )
-        mainLanguage.append(getElementsText(txtLanguage))
-        percentage.append(getElementsText(txtPercentage))
+        ).text
+        # mainLanguage.append(getElementsText(txtLanguage))
+        # percentage.append(getElementsText(txtPercentage))
         driver.save_screenshot("./evidence/" + url.split("IvanGaray/")[1] + ".png")
+        c.execute(
+            """INSERT INTO repositories VALUES(?,?,?)""",
+            (repoTitle, mainLanguage, percentage),
+        )
     except:
-        print("Something went wrong")
+        print("something went so wrong")
         driver.close()
 print("Finished!")
-
+conn.commit()
 # Making the structure using the data
-data_repositories = {
-    "title": titles,
-    "main language": mainLanguage,
-    "percentage": percentage,
-}
+# data_repositories = {
+#    "title": title,
+#    "main language": mainLanguage,
+#    "percentage": percentage,
+# }
 
-df = pd.DataFrame(data_repositories)
+# df = pd.DataFrame(data_repositories)
 
 # Save it into a CSV file
 df.to_csv("./evidence/repositories.csv")
+# df.to_csv("./evidence/repositories.csv")
 
 driver.close()
